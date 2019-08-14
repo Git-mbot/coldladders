4cf84808f2c81cf86d030e8242e04b2423f9e01b
micoobot <54089120+micoobot@users.noreply.github.com>
2019-08-14 08:12:16 -0400

    Update csb_c.php

--- csb_c.php
+++ csb_c.php
@@ -1,11 +1,11 @@
 <?php
-//remte database we pulls from to local
+//remote database we pull from to local
 $cs_link = mysqli_connect("host", "user", "pass", "db");
 if($cs_link === false){
     echo "There was an error connecting to REMOTE. <br>Error: ";
     die(mysqli_connect_error());
 }
-//local database our website stores & pulls from
+//local database our website stores & pull proccessed data from
 $mfdv_link = mysqli_connect("localhost", "use", "pass", "db");
 if($mfdv_link === false){
     echo "There was an error connecting to LOCAL. <br>Error: ";
